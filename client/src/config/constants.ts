cb8e6d7adc762b19f1a1302a0a082abf767d22ec
ryzx <rezzzyxz@gmail.com>
2025-08-11 17:12:49 +0700

    Update constants.ts

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -11,7 +11,7 @@ export const PORTFOLIO_CONFIG = {
   personal: {
     fullName: "Reza Arifa Rabbani",
     displayTitle: "RyezX", // Shown in navigation
-    location: "Sukabumi, Jawa Barat – Indonesia",
+    location: "Sukabumi, Jawa Barat",
     birthDate: "10 November 2010",
     status: "Pelajar", // Current status (e.g., "Student", "Freelancer", "Developer")
     passion: "Technology",
